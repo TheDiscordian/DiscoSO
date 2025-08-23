692a97d81cb6f8b2f3f9b3e0eaf1b80c4b0a27b3
riperiperi <rhy3756547@hotmail.com>
2025-08-23 17:41:14 +0100

    Add multithreading for 2D thumbnail loading
    (cherry picked from commit ef915b432ad3cda37b313c725722fc05c4fdf365)

--- TSOClient/tso.client/Rendering/City/LotThumbContent.cs
+++ TSOClient/tso.client/Rendering/City/LotThumbContent.cs
@@ -7,6 +7,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace FSO.Client.Rendering.City
 {
@@ -80,20 +81,10 @@ namespace FSO.Client.Rendering.City
                     result.LotTexture = DefaultThumb;
                     Client.GetThumbnailAsync(shardID, location, (data) =>
                     {
-                        if (data != null && !result.Dead && !result.Loaded)
+                        if (data != null && !result.Dead && !result.Loading)
                         {
-                            using (var mem = new MemoryStream(data))
-                            {
-                                result.Loaded = true;
-                                try
-                                {
-                                    result.LotTexture = ImageLoader.FromStream(GameFacade.GraphicsDevice, mem);
-                                }
-                                catch
-                                {
-                                    result.LotTexture = new Texture2D(GameFacade.GraphicsDevice, 1, 1);
-                                }
-                            }
+                            result.LoadTextureAsync(data);
+                            result.Loading = true;
                         }
                     });
                 }
@@ -218,7 +209,33 @@ namespace FSO.Client.Rendering.City
         public FSOF LotFacade;
         public int Held;
         public bool Loaded;
+        public bool Loading;
         public bool Dead;
         public bool FacadeEntry;
+
+        public void LoadTextureAsync(byte[] data)
+        {
+            Task.Run(() =>
+            {
+                using (var mem = new MemoryStream(data))
+                {
+                    Func<Texture2D> loader;
+                    try
+                    {
+                        loader = ImageLoader.NonUIFromStream(GameFacade.GraphicsDevice, mem);
+                    }
+                    catch
+                    {
+                        loader = () => new Texture2D(GameFacade.GraphicsDevice, 1, 1);
+                    }
+
+                    GameThread.InUpdate(() =>
+                    {
+                        LotTexture = loader?.Invoke() ?? new Texture2D(GameFacade.GraphicsDevice, 1, 1);
+                        Loaded = true;
+                    });
+                }
+            });
+        }
     }
 }
