88a9ed265e308db8d0be0abe064922f393a82d9c
TheDiscordian <43145244+TheDiscordian@users.noreply.github.com>
2026-07-28 19:25:27 -0400

    Restore System.Collections.Generic usings in VMRoomMap
    Upstream's version relies on .NET 9 implicit usings; we target net4.x.

--- TSOClient/tso.simantics/Model/VMRoomMap.cs
+++ TSOClient/tso.simantics/Model/VMRoomMap.cs
@@ -1,4 +1,7 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using Microsoft.Xna.Framework;
 using FSO.LotView.Model;
 using FSO.SimAntics.Model.Routing;
